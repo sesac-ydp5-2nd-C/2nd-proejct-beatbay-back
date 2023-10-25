b48f81394a34ef214563d516a5d151d039682619
flashrifle <flashrifle@gmail.com>
2023-10-25 13:22:58 +0900

    Refector: API 문서에 맞춰 user router 경로 수정

--- routes/user.js
+++ routes/user.js
@@ -4,8 +4,9 @@ const controller = require('../controller/UserController');
 
 router.get('/user', controller.getUser); // 유저 목록 조회
 
-router.get('/signup', controller.getSignup); // 회원가입 페이지 로드
-router.post('/signup', controller.signupUser); // 회원가입 폼 전송
+router.get('/user/signup', controller.getSignup); // 회원가입 페이지 로드
+router.post('/user/signup', controller.signupUser); // 회원가입 폼 전송
+
 router.post('/myPage/updateUser', controller.updateUser); // 회원정보 수정
 router.delete('/myPage/deleteUser', controller.deleteUser); // 회원탈퇴
 
