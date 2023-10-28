b88a9f652b2a916d592f67059f596dcebbf0a7f0
flashrifle <flashrifle@gmail.com>
2023-10-28 17:49:56 +0900

    Feat: 좋아요 기능을 위한 DB 수정

--- models/index.js
+++ models/index.js
@@ -16,6 +16,8 @@ const User = require('./User')(sequelize, Sequelize);
 const UserAuth = require('./UserAuth')(sequelize, Sequelize);
 const UsedProduct = require('./UsedProduct')(sequelize, Sequelize);
 const UsedAbility = require('./UsedAbility')(sequelize, Sequelize);
+const ProductFavorite = require('./ProductFavorite')(sequelize, Sequelize);
+const AbilityFavorite = require('./AbilityFavorite')(sequelize, Sequelize);
 
 // 유저 > 유저권한 외래키
 UserAuth.hasOne(User, {
@@ -24,30 +26,74 @@ UserAuth.hasOne(User, {
 });
 
 // User : UsedProduct = 1 : N
-User.hasMany(UsedProduct, {foreignKey: 'user_id',
-targetKey: 'user_id'});
-UsedProduct.belongsTo(User, {foreignKey: 'user_id', targetKey:'user_id'})
+User.hasMany(UsedProduct, { foreignKey: 'user_id', targetKey: 'user_id' });
+UsedProduct.belongsTo(User, { foreignKey: 'user_id', targetKey: 'user_id' });
 
 // User : UsedAbility = 1 : N
-User.hasMany(UsedAbility, {foreignKey: 'user_id',
-targetKey: 'user_id'});
-UsedAbility.belongsTo(User, {foreignKey: 'user_id', targetKey:'user_id'})
+User.hasMany(UsedAbility, { foreignKey: 'user_id', targetKey: 'user_id' });
+UsedAbility.belongsTo(User, { foreignKey: 'user_id', targetKey: 'user_id' });
 
 // UserAuth : UsedProduct = 1 : N
-UserAuth.hasMany(UsedProduct, {foreignKey: 'auth_id',
-targetKey: 'auth_id'});
-UsedProduct.belongsTo(UserAuth, {foreignKey: 'auth_id', targetKey:'auth_id'})
+UserAuth.hasMany(UsedProduct, { foreignKey: 'auth_id', targetKey: 'auth_id' });
+UsedProduct.belongsTo(UserAuth, {
+    foreignKey: 'auth_id',
+    targetKey: 'auth_id',
+});
 
 // UserAuth : UsedAbility = 1 : N
-UserAuth.hasMany(UsedAbility, {foreignKey: 'auth_id',
-targetKey: 'auth_id'});
-UsedAbility.belongsTo(UserAuth, {foreignKey: 'auth_id', targetKey:'auth_id'})
+UserAuth.hasMany(UsedAbility, { foreignKey: 'auth_id', targetKey: 'auth_id' });
+UsedAbility.belongsTo(UserAuth, {
+    foreignKey: 'auth_id',
+    targetKey: 'auth_id',
+});
+
+// User : ProductFavorite = 1 : N
+User.hasMany(ProductFavorite, {
+    foreignKey: 'user_id',
+    targetKey: 'user_id',
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE',
+});
+ProductFavorite.belongsTo(User, {
+    foreignKey: 'user_id',
+    targetKey: 'user_id',
+});
+
+// UsedProduct : ProductFavorite = 1 : N
+UsedProduct.hasMany(ProductFavorite, {
+    foreignKey: 'product_id',
+    targetKey: 'product_id',
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE',
+});
+
+// User : AbilityFavorite = 1 : N
+User.hasMany(AbilityFavorite, {
+    foreignKey: 'user_id',
+    targetKey: 'user_id',
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE',
+});
+AbilityFavorite.belongsTo(User, {
+    foreignKey: 'user_id',
+    targetKey: 'user_id',
+});
+
+// UsedAbility : AbilityFavorite = 1 : N
+UsedAbility.hasMany(AbilityFavorite, {
+    foreignKey: 'ability_id',
+    targetKey: 'ability_id',
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE',
+});
 
 // 모델 db 객체에 저장
 db.User = User;
 db.UserAuth = UserAuth;
 db.UsedProduct = UsedProduct;
 db.UsedAbility = UsedAbility;
+db.ProductFavorite = ProductFavorite;
+db.AbilityFavorite = AbilityFavorite;
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
