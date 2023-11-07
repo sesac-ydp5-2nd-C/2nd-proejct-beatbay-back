14f4995db83d77ffd07a6facae72219824bdbf38
flashrifle <flashrifle@gmail.com>
2023-11-07 21:09:28 +0900

    Refecotr: 필요 없는 라우터 삭제

--- routes/user.js
+++ routes/user.js
@@ -4,7 +4,6 @@ const controller = require('../controller/UserController');
 
 router.get('/', controller.getUser); // 유저 목록 조회
 
-router.get('/signup', controller.getSignup); // 회원가입 페이지 로드
 router.post('/signup', controller.signupUser); // 회원가입 폼 전송
 router.post('/idExists', controller.idCheck); // 아이디 확인
 
