068fb95bcc8c915de6ce2eb6c5b16d78752ddf6b
pipi-shortstocking <jannie435@naver.com>
2023-11-17 00:44:25 +0900

    Feat: 모델 생성 중(#127)

---
+++ models/Column.js
@@ -0,0 +1,7 @@
+const { DataTypes } = require('sequelize');
+const Cokumn = (Sequelize, sequelize) => {
+    const model = Sequelize.define('column', {
+        // 제목, 내용
+        // 작성자, 날짜 -> 상의
+    });
+};
---
+++ models/Notice.js
@@ -0,0 +1,7 @@
+const { DataTypes } = require('sequelize');
+const Notice = (Sequelize, sequelize) => {
+    const model = Sequelize.define('notice', {
+        // 제목, 내용
+        // 작성자, 날짜 -> 상의
+    });
+};
